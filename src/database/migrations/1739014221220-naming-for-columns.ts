ef2dc31bab6ada9d8cc7f603a48fba97853a8da7
Maksym Vakulenko <vakulenko.maksim977@gmail.com>
2025-06-02 00:44:05 +0300

    chore: update migration for renaming columns

--- src/database/migrations/1739014221220-naming-for-columns.ts
+++ src/database/migrations/1739014221220-naming-for-columns.ts
@@ -14,21 +14,9 @@ export class NamingForColumns1739014221220 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "itineraries" ADD "end_date" TIMESTAMP NOT NULL`,
     );
-    await queryRunner.query(
-      `ALTER TABLE "users_to_itinerary" ADD CONSTRAINT "FK_cae23439cc5ce1032639737a9bc" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "users_to_itinerary" ADD CONSTRAINT "FK_31c849ffd8a039ab79a5c48df8c" FOREIGN KEY ("itineraryId") REFERENCES "itineraries"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "users_to_itinerary" DROP CONSTRAINT "FK_31c849ffd8a039ab79a5c48df8c"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "users_to_itinerary" DROP CONSTRAINT "FK_cae23439cc5ce1032639737a9bc"`,
-    );
     await queryRunner.query(`ALTER TABLE "itineraries" DROP COLUMN "end_date"`);
     await queryRunner.query(
       `ALTER TABLE "itineraries" DROP COLUMN "start_date"`,
